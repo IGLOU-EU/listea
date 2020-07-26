5a45af9c8b5a45255392a459fdb13033b57b028c
adrien <adrien@iglou.eu>
2020-07-26 04:26:37 +0200

    API url builder
    Signed-off-by: adrien <adrien@iglou.eu>

--- main.go
+++ main.go
@@ -14,16 +14,22 @@ import (
 type Config struct {
 	APIURL   string `json:"api_url"`
 	APIToken string `json:"api_token"`
-	List     []struct {
-		APIRequest string `json:"api_request"`
-		QueryKey   struct {
-			Q          string `json:"q"`
-			Type       string `json:"type"`
-			State      string `json:"state"`
-			Labels     string `json:"labels"`
-			Milestones string `json:"milestones"`
-		} `json:"query_key"`
-	} `json:"list"`
+	List     []List `json:"list"`
+}
+
+// QueryKey inline type def
+type QueryKey struct {
+	Q          string `json:"q"`
+	Type       string `json:"type"`
+	State      string `json:"state"`
+	Labels     string `json:"labels"`
+	Milestones string `json:"milestones"`
+}
+
+// List inline type def
+type List struct {
+	APIRequest string   `json:"api_request"`
+	QueryKey   QueryKey `json:"query_key"`
 }
 
 var apiRequest []string
@@ -40,9 +46,62 @@ func init() {
 		log.Fatal(err)
 	}
 
-	fmt.Println(config)
+	apiRequest = buildAPIRequest(config)
+	fmt.Println(apiRequest)
 }
 
 func main() {
 
+}
+
+func buildAPIRequest(config Config) []string {
+	var list []string
+
+	for _, v := range config.List {
+		list = append(list,
+			fmt.Sprintf(
+				"%s%s?%stoken=%%20%%20%s",
+				config.APIURL,
+				v.APIRequest,
+				buildAPIQuery(v.QueryKey),
+				config.APIToken,
+			),
+		)
+	}
+
+	return list
+}
+
+func buildAPIQuery(query QueryKey) string {
+	o := ""
+
+	if !empty(query.Q) {
+		o += fmt.Sprintf("q=%s&", query.Q)
+	}
+
+	if !empty(query.Type) {
+		o += fmt.Sprintf("type=%s&", query.Type)
+	}
+
+	if !empty(query.State) {
+		o += fmt.Sprintf("state=%s&", query.State)
+	}
+
+	if !empty(query.Labels) {
+		o += fmt.Sprintf("labels=%s&", query.Labels)
+	}
+
+	if !empty(query.Milestones) {
+		o += fmt.Sprintf("milestones=%s&", query.Milestones)
+	}
+
+	return o
+}
+
+func empty(v string) bool {
+	if v != "" {
+		return true
+	}
+
+	return false
 }
