0ae228d60fcbfd87b942e2535bc88d6a71633b29
adrien <adrien@iglou.eu>
2020-07-26 03:48:15 +0200

    Init function && Read config file
    Signed-off-by: adrien <adrien@iglou.eu>

--- main.go
+++ main.go
@@ -3,8 +3,46 @@
 
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+)
+
+// Config inline type def
+type Config struct {
+	APIURL   string `json:"api_url"`
+	APIToken string `json:"api_token"`
+	List     []struct {
+		APIRequest string `json:"api_request"`
+		QueryKey   struct {
+			Q          string `json:"q"`
+			Type       string `json:"type"`
+			State      string `json:"state"`
+			Labels     string `json:"labels"`
+			Milestones string `json:"milestones"`
+		} `json:"query_key"`
+	} `json:"list"`
+}
+
+var apiRequest []string
+
+func init() {
+	cf, err := ioutil.ReadFile("config")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var config Config
+	err = json.Unmarshal(cf, &config)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(config)
+}
 
 func main() {
-    fmt.Println("hello world")
+
 }
